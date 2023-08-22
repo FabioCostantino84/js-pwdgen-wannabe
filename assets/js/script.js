a353e941b815a5a38cff704c7b1537ffd9e2c5eb
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-22 15:38:55 +0200

    prompt name

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,9 @@ ed infine scrivi in pagina (non solo in console) il risultato,
 che va formattato in questo modo: 
 nomecognomecolorepreferito23  */ 
 
+
+const userName = prompt('type your name');
+console.log(userName);
+
+document.getElementById('name').innerHTML = userName;
+
